c793497a524add24cbb68813e62939bf1aa65984
Julian Liu <wlootlxt123@gmail.com>
2020-07-20 23:50:58 +0800

    13

--- js/src/forum/renderEmoji.js
+++ js/src/forum/renderEmoji.js
@@ -10,7 +10,7 @@ export default function renderEmoji() {
     const contentHtml = original();
 
     if (this.oldContentHtml !== contentHtml) {
-      const buildUrl = 'https://cdn.jsdelivr.net/gh/twitter/twemoji@12.1.2/assets/';
+      const buildUrl = 'https://cdn.jsdelivr.net/gh/twitter/twemoji@13.0.0/assets/';
       this.emojifiedContentHtml = twemoji.parse(contentHtml, { base: buildUrl });
       this.oldContentHtml = contentHtml;
     }
@@ -21,7 +21,7 @@ export default function renderEmoji() {
   override(s9e.TextFormatter, 'preview', (original, text, element) => {
     original(text, element);
 
-    const buildUrl = 'https://cdn.jsdelivr.net/gh/twitter/twemoji@12.1.2/assets/';
+    const buildUrl = 'https://cdn.jsdelivr.net/gh/twitter/twemoji@13.0.0/assets/';
     twemoji.parse(element, { base: buildUrl });
   });
 }
